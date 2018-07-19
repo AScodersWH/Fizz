ca8fccaf87ac2dc6d8261c0f9d07c942e364ce36
AScodersWH <895052253@qq.com>
2018-07-19 13:06:00 +0800

    [master] finish this

--- Fizz/Fizz.js
+++ Fizz/Fizz.js
@@ -1,11 +1,14 @@
 'use strict'
-var studentLine = [];
 
 function count() {
     for (var item = 1; item <= 120; item++) {
-        studentLine.push(item);
+        var result = "";
+        if(item % 3 == 0) result += "Fizz";
+        if(item % 5 == 0) result += "Buzz";
+        if(item % 7 == 0) result += "Whizz"
+        if(item % 3 == 0 || item % 5 == 0 || item % 7 == 0) console.log(result);
+        else console.log(item);
     }
-    console.log(studentLine);
 }
 
 count();
